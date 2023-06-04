d0694c145b7553df7f5846362128e832c1e8e39e
Breno <breno.ssouza@sptech.school>
2023-06-03 23:56:30 -0300

    api conectada + novo css no quiz

---
+++ site/src/database/script-spfc-tabelas.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE SPFC;
+
+USE SPFC;
+
+CREATE TABLE usuario (
+idUsuario int primary key auto_increment,
+nome varchar (45),
+email varchar(90),
+senha varchar(90)
+);
+
+
+
+
+
+
+
+
