9cbeefeb27ea1be122794c19c85d5926dfea193f
Breno <breno.ssouza@sptech.school>
2023-06-10 13:46:20 -0300

    novo carrosel de titulos

--- site/src/database/script-spfc-tabelas.sql
+++ site/src/database/script-spfc-tabelas.sql
@@ -9,6 +9,27 @@ email varchar(90),
 senha varchar(90)
 );
 
+select * from usuario;
+
+CREATE TABLE quiz (
+idQuiz int primary key auto_increment,
+score int
+);
+
+CREATE TABLE ranking (
+idRanking int,
+fkUsuario int,
+fkQuiz int,
+foreign key (fkUsuario) references usuario(idUsuario),
+foreign key (fkQuiz) references quiz(idQuiz),
+primary key (idRanking, fkUsuario, fkQuiz)
+);
+
+
+
+
+
+
 
 
 
